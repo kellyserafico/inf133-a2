75429df5df263625c0ddd5c9741f40561782b1e8
kellyserafico <114453810+kellyserafico@users.noreply.github.com>
2023-10-24 12:27:45 -0700

    added dates

--- js/about.js
+++ js/about.js
@@ -11,10 +11,23 @@ function parseTweets(runkeeper_tweets) {
 	
 	//This line modifies the DOM, searching for the tag with the numberTweets ID and updating the text.
 	//It works correctly, your task is to update the text of the other tags in the HTML file!
+	//TODO: convert to jQuery
+	
 	document.getElementById('numberTweets').innerText = tweet_array.length;	
+	document.getElementById('firstDate').innerText = formatDate(tweet_array[tweet_array.length - 2]["time"]);
+	document.getElementById("lastDate").innerText = formatDate(tweet_array[0]["time"]);
+	
+	console.log(tweet_array[0].source());
 }
 
 //Wait for the DOM to load
 document.addEventListener('DOMContentLoaded', function (event) {
 	loadSavedRunkeeperTweets().then(parseTweets);
 });
+
+function formatDate(date){
+	let words = date.toString().split(' ');
+	let formatDate = words[1] + " " + words[2] + ", " + words[3];
+	return formatDate;
+	
+}
