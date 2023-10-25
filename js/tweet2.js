1b162bb52b10c722abee8c46815ee736225192f8
kellyserafico <114453810+kellyserafico@users.noreply.github.com>
2023-10-25 14:35:47 -0700

    a

--- js/tweet2.js
+++ js/tweet2.js
@@ -1,51 +1,51 @@
+"use strict";
 class Tweet {
-	private text:string;
-	time:Date;
-
-	constructor(tweet_text:string, tweet_time:string) {
+    constructor(tweet_text, tweet_time) {
         this.text = tweet_text;
-		this.time = new Date(tweet_time);//, "ddd MMM D HH:mm:ss Z YYYY"
-	}
-
-	//returns either 'live_event', 'achievement', 'completed_event', or 'miscellaneous'
-    get source():string {
+        this.time = new Date(tweet_time); //, "ddd MMM D HH:mm:ss Z YYYY"
+    }
+    //returns either 'live_event', 'achievement', 'completed_event', or 'miscellaneous'
+    get source() {
         //TODO: identify whether the source is a live event, an achievement, a completed event, or miscellaneous.
         return "unknown";
     }
-
     //returns a boolean, whether the text includes any content written by the person tweeting.
-    get written():boolean {
+    get written() {
         //TODO: identify whether the tweet is written
         return false;
     }
-
-    get writtenText():string {
-        if(!this.written) {
+    get writtenText() {
+        if (!this.written) {
             return "";
         }
         //TODO: parse the written text from the tweet
         return "";
     }
-
-    get activityType():string {
+    get activityType() {
         if (this.source != 'completed_event') {
             return "unknown";
         }
         //TODO: parse the activity type from the text of the tweet
         return "";
     }
-
-    get distance():number {
-        if(this.source != 'completed_event') {
+    get distance() {
+        if (this.source != 'completed_event') {
             return 0;
         }
-        //TODO: prase the distance from the text of the twee
+        //TODO: prase the distance from the text of the tweet
         return 0;
     }
-
-    getHTMLTableRow(rowNumber:number):string {
+    getHTMLTableRow(rowNumber) {
         //TODO: return a table row which summarizes the tweet with a clickable link to the RunKeeper activity
-        return "<tr>hai</tr>";
-        
+        let row = document.createElement("tr");
+
+        let cell1 = row.insertCell(0);
+        let cell2 = row.insertCell(1);
+        let cell3 = row.insertCell(2);
+
+        cell1.innerHTML = rowNumber;
+        cell2.innerHTML = this.activityType;
+        cell3.innerHTML = this.text;
+        return row;
     }
 }
