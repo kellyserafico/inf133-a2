786280cfee30ad39643e75c348364f3edee6529d
kellyserafico <114453810+kellyserafico@users.noreply.github.com>
2023-10-25 22:58:45 -0700

    top three activities

--- js/activities.js
+++ js/activities.js
@@ -27,12 +27,28 @@ function parseTweets(runkeeper_tweets) {
 				}
 			}
 		}
-
 	});
+	let topThree = findTopThree(frequency);
+	let firstMost = secondMost = thirdMost = "";
+	console.log(firstMost)
+	for(let i = 0; i < frequency.length; i++){
+		if (topThree[0] == frequency[i][1]){
+			firstMost = frequency[i][0];
+		}
+		else if(topThree[1] == frequency[i][1]){
+			secondMost = frequency[i][0];
+		}
+		else if(topThree[2] == frequency[i][1]){
+			thirdMost = frequency[i][0];
+		}
+	}
+	
+	
 	
 	document.getElementById("numberActivities").innerText = activities.length
-	let firstMost = document.getElementById("firstMost");
-	console.log((frequency))
+	document.getElementById("firstMost").innerText = firstMost;
+	document.getElementById("secondMost").innerText = secondMost;
+	document.getElementById("thirdMost").innerText = thirdMost;
 
 
 	//TODO: create a new array or manipulate tweet_array to create a graph of the number of tweets containing each type of activity.
@@ -50,6 +66,19 @@ function parseTweets(runkeeper_tweets) {
 	//TODO: create the visualizations which group the three most-tweeted activities by the day of the week.
 	//Use those visualizations to answer the questions about which activities tended to be longest and when.
 }
+
+function findTopThree(freq){
+	let nums = [];
+	for(let i = 0; i < freq.length; i++){
+		nums[i] = freq[i][1];
+	}
+	nums = nums.sort(function (a, b) {  return a - b;  }).reverse();
+	return [nums[0], nums[1], nums[2]]
+}
+
+
+
+
 
 //Wait for the DOM to load
 document.addEventListener('DOMContentLoaded', function (event) {
